5cd3328ee7a8172af8da719e8b21e6f260a9b71f
Elliott Sprehn <esprehn@chromium.org>
2014-02-25 11:00:20 -0800

    Move resource related things from base.js to resources.js

--- ui/resources.js
+++ ui/resources.js
@@ -1,4 +1,41 @@
 
+function loadResource(type, url)
+{
+    return new Promise(function(fulfill, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.responseType = type;
+        xhr.open("GET", url);
+        xhr.send();
+        xhr.onload = function() {
+            fulfill(xhr);
+        };
+        xhr.onerror = function(e) {
+            reject(xhr);
+        };
+    });
+}
+
+function loadText(url)
+{
+    return loadResource("text", url).then(function(xhr) {
+        return xhr.responseText;
+    });
+}
+
+function loadDocument(url)
+{
+    return loadResource("document", url).then(function(xhr) {
+        return xhr.responseXML;
+    });
+}
+
+function loadJSON(url)
+{
+    return loadResource("json", url).then(function(xhr) {
+        return xhr.response;
+    });
+}
+
 function createImage(name)
 {
     return new Image(chrome.extension.getURL("ui/images/" + name));
