b521680121f61cbae04b2257c8ce3305138856f7
Elliott Sprehn <esprehn@chromium.org>
2014-03-04 16:33:44 -0800

    Redirect from http properly

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-    if (details.url === "https://codereview.chromium.org/")
+    if (details.url === "https://codereview.chromium.org/"
+        || details.url === "http://codereview.chromium.org/")
         chrome.tabs.update(details.tabId, {url:"https://codereview.chromium.org/app/"});
 });
