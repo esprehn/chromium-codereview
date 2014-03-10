1f8061619828d3dbc2629f7f1065b9fef580ddc1
Elliott Sprehn <esprehn@chromium.org>
2014-03-10 14:16:49 -0700

    Make Element#remove() work in Shadow DOM polyfill

--- ui/base.js
+++ ui/base.js
@@ -3,3 +3,10 @@ function toArray(value)
 {
     return Array.prototype.slice.call(value);
 }
+
+// Manually implement this so that Shadow DOM polyfill works.
+// https://github.com/Polymer/ShadowDOM/issues/395
+Element.prototype.remove = function()
+{
+    this.parentNode.removeChild(this);
+}
