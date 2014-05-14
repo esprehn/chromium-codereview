3f6d81b7b49299b38a6c1b5e0dbfa83c79fc4f0f
Elliott Sprehn <esprehn@chromium.org>
2014-05-14 10:40:53 -0500

    Fix import ordering now that imports run async when dynamically inserted

--- ui/main.js
+++ ui/main.js
@@ -10,12 +10,11 @@ onload = function() {
         "<meta name=viewport content='width=device-width, user-scalable=no'>" +
         "<title>Chromium Code Review</title>" +
         "<script src='" + resolveUrl("bower_components/platform/platform.js") + "'></script>" +
+        "<link rel='import' href='" + resolveUrl("bower_components/polymer/polymer.html") + "'>" +
+        "<link rel='import' href='" + resolveUrl("ui/components/cr-app.html") + "'>" +
+        "<link rel='stylesheet' href='" + resolveUrl("ui/style.css") + "'>" +
         "<cr-app></cr-app>"
     );
     document.close();
-
     document.documentElement.style.display = '';
-    document.head.appendChild(createLink("import", "bower_components/polymer/polymer.html"));
-    document.head.appendChild(createLink("import", "ui/components/cr-app.html"));
-    document.head.appendChild(createLink("stylesheet", "ui/style.css"));
 };
