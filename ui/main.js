da10e6e5f2aa790dac89e0f8b4a1d5ffe631caf1
Elliott Sprehn <esprehn@chromium.org>
2014-06-07 19:11:08 -0700

    Show an error when experimental web platform features is disabled. Fixes #9.

--- ui/main.js
+++ ui/main.js
@@ -5,16 +5,23 @@ document.documentElement.style.display = 'none';
 // Can't use DOMContentLoaded, calling document.write or document.close inside it from
 // inside an extension causes a crash.
 onload = function() {
-    document.write(
-        "<!DOCTYPE html>" +
-        "<meta name=viewport content='width=device-width, user-scalable=no'>" +
-        "<title>Chromium Code Review</title>" +
-        "<script src='" + resolveUrl("bower_components/platform/platform.js") + "'></script>" +
-        "<link rel='import' href='" + resolveUrl("bower_components/polymer/polymer.html") + "'>" +
-        "<link rel='import' href='" + resolveUrl("ui/components/cr-app.html") + "'>" +
-        "<link rel='stylesheet' href='" + resolveUrl("ui/style.css") + "'>" +
-        "<cr-app></cr-app>"
-    );
+    if (!window.HTMLDialogElement) {
+        document.write(
+            "<!DOCTYPE html>" +
+            "<h1>Please enable experimental web platform features in about:flags.</h1>"
+        );
+    } else {
+        document.write(
+            "<!DOCTYPE html>" +
+            "<meta name=viewport content='width=device-width, user-scalable=no'>" +
+            "<title>Chromium Code Review</title>" +
+            "<script src='" + resolveUrl("bower_components/platform/platform.js") + "'></script>" +
+            "<link rel='import' href='" + resolveUrl("bower_components/polymer/polymer.html") + "'>" +
+            "<link rel='import' href='" + resolveUrl("ui/components/cr-app.html") + "'>" +
+            "<link rel='stylesheet' href='" + resolveUrl("ui/style.css") + "'>" +
+            "<cr-app></cr-app>"
+        );
+    }
     document.close();
     document.documentElement.style.display = '';
 };
