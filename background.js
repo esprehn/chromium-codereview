54d9d414c1fd2a4fdfabc91bc9199f5a20862845
Elliott Sprehn <esprehn@chromium.org>
2014-06-08 22:13:36 -0700

    Redirect chromiumcodereview.appspot.com to codereview.chromium.org. Fixes #22.

--- background.js
+++ background.js
@@ -1,10 +1,18 @@
 
-var URL_PATTERN = /^https?:\/\/codereview.chromium.org\/(\d+)?\/?$/;
+var APPSPOT_URL_PATTERN = /^https?:\/\/chromiumcodereview\.appspot\.com/;
+var APP_REDIRECT_URL_PATTERN = /^https?:\/\/codereview\.chromium\.org\/(\d+)?\/?$/;
+
+var CHROMIUM_URL = "https://codereview.chromium.org";
 var APP_URL = "https://codereview.chromium.org/static/app/";
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
     var url = details.url;
-    var match = url.match(URL_PATTERN);
+    if (url.match(APPSPOT_URL_PATTERN)) {
+        var url = url.replace(APPSPOT_URL_PATTERN, CHROMIUM_URL);
+        chrome.tabs.update(details.tabId, {url:url});
+        return;
+    }
+    var match = url.match(APP_REDIRECT_URL_PATTERN);
     if (!match)
         return;
     var issueId = parseInt(match[1], 10);
