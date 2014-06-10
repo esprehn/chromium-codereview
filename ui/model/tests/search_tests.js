896e8245b0f9edd4d849b2f1ef98ac84f0d275b0
Elliott Sprehn <esprehn@chromium.org>
2014-06-10 12:35:46 -0700

    Fix style of search tests.

--- ui/model/tests/search_tests.js
+++ ui/model/tests/search_tests.js
@@ -1,18 +1,18 @@
 
-describe("Search should", function() {
-    asyncIt("encode url parameters", function() {
+describe("Search", function() {
+    asyncIt("should encode url parameters", function() {
         loadText.expect("/account?limit=10&q=sp%20%3Are", function() {
             return Promise.resolve("");
         });
         return Search.findUsers("sp :re", "not a number");
     });
-    asyncIt("pass a default limit", function() {
+    asyncIt("should pass a default limit", function() {
         loadText.expect("/account?limit=10&q=esprehn", function() {
             return Promise.resolve("");
         });
         return Search.findUsers("esprehn");
     });
-    asyncIt("sort users by email", function() {
+    asyncIt("should sort users by email", function() {
         loadText.expect("/account?limit=10&q=ojan", function() {
             return Promise.resolve("ojan@chromium.org (Ojan)\nesprehn@chromium.org (Elliott)");
         });
