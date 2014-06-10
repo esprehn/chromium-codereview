dffe4440dd99fee24c1954d3731241a38934a433
Elliott Sprehn <esprehn@chromium.org>
2014-06-10 12:09:05 -0700

    Make async jasmine tests prettier.

--- ui/jasmine.js
+++ ui/jasmine.js
@@ -1,20 +1,26 @@
 
-function AsyncTest()
+function asyncIt(name, fn)
 {
-    var done = false;
-
-    this.wait = function() {
+    it(name, function() {
+        var test = this;
+        var done = false;
+        function finish(error) {
+            done = true;
+            if (error)
+                test.fail(error);
+        }
+        var result = fn.call(this, function(result) {
+            finish(result);
+        });
+        if (result instanceof Promise) {
+            result.then(function() {
+                finish();
+            }, function(error) {
+                test.fail(error);
+            });
+        }
         waitsFor(function() {
             return done;
         }, 50);
-    };
-
-    this.done = function() {
-        done = true;
-    };
-
-    this.fail = function(error) {
-        done = true;
-        throw new Error(error);
-    };
+    });
 }
--- ui/model/tests/search_tests.js
+++ ui/model/tests/search_tests.js
@@ -1,49 +1,22 @@
 
 describe("Search should", function() {
-    it("encode url parameters", function() {
-        var async = new AsyncTest();
+    asyncIt("encode url parameters", function() {
         loadText.expect("/account?limit=10&q=sp%20%3Are", function() {
             return Promise.resolve("");
         });
-        runs(function() {
-            Search.findUsers("sp :re", "not a number").then(function() {
-                async.done();
-            }).catch(function(e) {
-                async.fail(e);
-            });
-        });
-        async.wait();
+        return Search.findUsers("sp :re", "not a number");
     });
-    it("pass a default limit", function() {
-        var async = new AsyncTest();
+    asyncIt("pass a default limit", function() {
         loadText.expect("/account?limit=10&q=esprehn", function() {
             return Promise.resolve("");
         });
-        runs(function() {
-            Search.findUsers("esprehn").then(function() {
-                async.done();
-            }).catch(function(e) {
-                async.fail(e);
-            });
-        });
-        async.wait();
+        return Search.findUsers("esprehn");
     });
-    it("sort users by email", function() {
-        var async = new AsyncTest();
+    asyncIt("sort users by email", function() {
         loadText.expect("/account?limit=10&q=ojan", function() {
             return Promise.resolve("ojan@chromium.org (Ojan)\nesprehn@chromium.org (Elliott)");
         });
-        var users;
-        runs(function() {
-            Search.findUsers("ojan", 10).then(function(value) {
-                users = value;
-                async.done();
-            }).catch(function(e) {
-                async.fail(e);
-            });
-        });
-        async.wait();
-        runs(function() {
+        return Search.findUsers("ojan", 10).then(function(users) {
             expect(users).toEqual([
                 new User("Elliott", "esprehn@chromium.org", "esprehn"),
                 new User("Ojan", "ojan@chromium.org", "ojan"),
