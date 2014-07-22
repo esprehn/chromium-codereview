e4910f6b532536c2331e60a5dfb0950edba04b4f
Elliott Sprehn <esprehn@chromium.org>
2014-07-21 17:28:07 -0700

    Support multiple trackers in BUG= lines. Fixes #83.

--- ui/link_text_parser.js
+++ ui/link_text_parser.js
@@ -4,11 +4,32 @@ function LinkTextParser(callback)
     this.callback = callback;
 }
 
+LinkTextParser.TRACKERS = [
+    "chromium",
+    "chromium-os",
+    "chrome-os-partner",
+    "gyp",
+    "skia",
+    "v8"
+];
+
 LinkTextParser.prototype.addText = function(text, href)
 {
     if (!text)
         return;
     this.callback(text, href);
+};
+
+LinkTextParser.prototype.addBugText = function(text, tracker, bugId)
+{
+    if (tracker && !LinkTextParser.TRACKERS.find(tracker)) {
+        this.addText(text)
+        return;
+    }
+    var href = "http://code.google.com/p/{1}/issues/detail?id={2}".assign(
+        encodeURIComponent(tracker || "chromium"),
+        encodeURIComponent(bugId));
+    this.addText(text, href);
 };
 
 LinkTextParser.prototype.parse = function(text)
@@ -28,22 +49,25 @@ LinkTextParser.prototype.parseChunk = function(text, href)
 };
 
 LinkTextParser.prototype.parseBugs = function(text) {
-    var BUG_PATTERN = /^BUG=(\d+|,|\s)+$/m;
-    var BUG_ID = /^(.*?)(\d+)(.*?)$/m;
+    var BUG_PATTERN = /^BUG=(.+)$/m;
+    var BUG_ID = /(([a-zA-Z0-9\-]*):)?(\d+)/;
 
     for (var match = text.match(BUG_PATTERN); match; match = text.match(BUG_PATTERN)) {
         var before = text.substring(0, match.index);
         this.addText(before);
+        this.addText("BUG=");
         text = text.substring(match.index + match[0].length);
-        var bugText = match[0];
+        var bugText = match[1];
         for (var bugMatch = bugText.match(BUG_ID); bugMatch; bugMatch = bugText.match(BUG_ID)) {
-            this.addText(bugMatch[1]);
-            var bugId = parseInt(bugMatch[2], 10);
-            this.addText(bugId, "http://crbug.com/" + bugId);
-            bugText = bugMatch[3] || "";
+            var before = bugText.substring(0, bugMatch.index);
+            this.addText(before);
+            var tracker = bugMatch[2];
+            var bugId = bugMatch[3];
+            var matchText = bugMatch[0];
+            this.addBugText(matchText, tracker, bugId);
+            bugText = bugText.substring(bugMatch.index + bugMatch[0].length);
         }
+        this.addText(bugText);
     }
-
-    // Add the remainder.
     this.addText(text);
 };
--- ui/link_text_parser_tests.js
+++ ui/link_text_parser_tests.js
@@ -32,18 +32,48 @@ describe("LinkTextParser", function() {
         expectTokens("abc\n baz \nBUG=123 \nfoo bar", [
             "abc\n baz \n",
             "BUG=",
-            {href:"http://crbug.com/123", text:"123"},
+            {href:"http://code.google.com/p/chromium/issues/detail?id=123", text:"123"},
+            " ",
             "\nfoo bar",
         ]);
     });
     it("should parse links and BUG= lines", function() {
-        expectTokens("abc\n http://www.google.com/ baz \nBUG=123 \nfoo bar", [
+        expectTokens("abc\n http://www.google.com/ baz \nBUG=456 \nfoo bar", [
             "abc\n ",
             {href:"http://www.google.com/", text:"http://www.google.com/"},
             " baz \n",
             "BUG=",
-            {href:"http://crbug.com/123", text:"123"},
+            {href:"http://code.google.com/p/chromium/issues/detail?id=456", text:"456"},
+            " ",
             "\nfoo bar",
+        ]);
+    });
+    it("should parse multiple bugs in a BUG= line", function() {
+        expectTokens("abc\nBUG=456, 678", [
+            "abc\n",
+            "BUG=",
+            {href:"http://code.google.com/p/chromium/issues/detail?id=456", text:"456"},
+            ", ",
+            {href:"http://code.google.com/p/chromium/issues/detail?id=678", text:"678"},
+        ]);
+    });
+    it("should parse multiple bugs in a BUG= line", function() {
+        expectTokens("abc\nBUG=456,   v8:678,chromium-os:123456", [
+            "abc\n",
+            "BUG=",
+            {href:"http://code.google.com/p/chromium/issues/detail?id=456", text:"456"},
+            ",   ",
+            {href:"http://code.google.com/p/v8/issues/detail?id=678", text:"v8:678"},
+            ",",
+            {href:"http://code.google.com/p/chromium-os/issues/detail?id=123456", text:"chromium-os:123456"},
+        ]);
+    });
+    it("should ignore invalid trackers in a BUG= line", function() {
+        expectTokens("abc\nBUG=foo:678\nbar", [
+            "abc\n",
+            "BUG=",
+            "foo:678",
+            "\nbar",
         ]);
     });
 });
