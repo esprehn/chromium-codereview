d6d523e79b3fb1f84d8083b62d861dd49cb60e71
Elliott Sprehn <esprehn@chromium.org>
2014-07-27 10:30:58 -0700

    Don't override Element#remove if the browser supports it.

--- ui/base.js
+++ ui/base.js
@@ -6,17 +6,17 @@ function toArray(value)
 
 // Manually implement this so that Shadow DOM polyfill works.
 // https://github.com/Polymer/ShadowDOM/issues/395
-Element.prototype.remove = function()
-{
-    if (!this.parentNode)
-        return;
-    this.parentNode.removeChild(this);
+if (!Element.prototype.remove) {
+    Element.prototype.remove = function() {
+        if (!this.parentNode)
+            return;
+        this.parentNode.removeChild(this);
+    }
 }
 
 // We only have String#contains if "Experimental JavaScript is enabled."
 if (!String.prototype.contains) {
-    String.prototype.contains = function(text)
-    {
+    String.prototype.contains = function(text) {
         return this.indexOf(text) != -1;
     }
 }
