043a1296629909d541b53369f4967175d67c8ebf
Elliott Sprehn <esprehn@chromium.org>
2014-08-29 14:26:41 -0700

    Remove dom.js, it was never really used.

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,18 @@ document.documentElement.style.display = 'none';
 // Can't use DOMContentLoaded, calling document.write or document.close inside it from
 // inside an extension causes a crash.
 window.addEventListener("load", function() {
+    function resolveUrl(name) {
+        if (!chrome || !chrome.extension)
+            return name;
+        return chrome.extension.getURL(name);
+    }
+
     document.write(
         "<!DOCTYPE html>" +
         "<meta name=viewport content='initial-scale=1, maximum-scale=1, user-scalable=no'>" +
         "<title>Chromium Code Review</title>" +
         "<script src='" + resolveUrl("bower_components/platform/platform.js") + "'></script>" +
-        "<script>EXTENSION_URL = '" + chrome.extension.getURL("") + "';</script>" +
+        "<script>EXTENSION_URL = '" + resolveUrl("") + "';</script>" +
         "<link rel='import' href='" + resolveUrl("ui/components/cr-app.html") + "'>" +
         "<link rel='stylesheet' href='" + resolveUrl("ui/style.css") + "'>" +
         "<cr-app></cr-app>"
